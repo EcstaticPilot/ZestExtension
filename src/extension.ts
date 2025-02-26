1ed985ca97122fe547cb4f7964421a58b5daa8b8
Nikhil Ramanuja <Nikhil.ramanuja.8@gmail.com>
2025-02-26 10:00:25 -0800

    more placeholdert commands

--- src/extension.ts
+++ src/extension.ts
@@ -13,13 +13,20 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('zestcode.helloWorld', () => {
+	const helloWorld = vscode.commands.registerCommand('zestcode.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ZestCode!');
+		vscode.window.showInformationMessage('Hello World from ZestCode!!');
 	});
-
-	context.subscriptions.push(disposable);
+	const build = vscode.commands.registerCommand('zestcode.build', () => {
+		vscode.window.showInformationMessage('Building the project...');
+	}
+	);
+	const run = vscode.commands.registerCommand('zestcode.run', () => {
+		vscode.window.showInformationMessage('Running the project...');
+	}
+	);
+	context.subscriptions.push(helloWorld,build,run);
 }
 
 // This method is called when your extension is deactivated
